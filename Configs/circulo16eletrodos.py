319102a1a5bd3acf1547d2924659f49dc9ccd0f4
rodgraneiro <40045555+rodgraneiro@users.noreply.github.com>
2025-08-24 22:23:54 -0300

    adicionando novos diretorios

---
+++ Configs/circulo16eletrodos.py
@@ -0,0 +1,7 @@
+nome_arquivo = "circulo16eletrodos" 
+raio = 0.15
+n_eletrodos = 16
+ponto_medio_x = 0
+ponto_medio_y = 0
+lc1 = 1.0e-2
+lc2 = 1.0e-2
---
+++ Configs/circulo32eletrodos.py
@@ -0,0 +1,7 @@
+nome_arquivo = "circulo32eletrodos" 
+raio = 0.15
+n_eletrodos = 32
+ponto_medio_x = 0
+ponto_medio_y = 0
+lc1 = 1.0e-2
+lc2 = 1.0e-2
