6aa33d593cb5c76e6009d59cfe6c786b88ac3032
rodgraneiro <rodgraneiro@gmail.com>
2025-08-27 20:39:08 -0300

    Update EIT_mesh_2D.py
    novo

--- EIT_mesh_2D.py
+++ EIT_mesh_2D.py
@@ -9,12 +9,13 @@ import numpy as np
 def files_mesh(opcao):
     caminho_malhas = {
     '1' : r'octogono_2208_mod.msh',     # malha elem de tamanhos diferentes
-    '2' : r'DoisTriangulos_4elem.msh', # DoisTriangulos  4 elementos    
+    '2' : r'DoisTriangulos_4elem.msh', # DoisTriangulos  4 elementos   
     # '100' : r'd:/ParaGitHub/Estudo-EIT/unidimensional_100e_py.msh',
     }
     sigma_reais = {
         '1': np.concatenate([np.full(128, 0.1), np.full(18, 0.75)]),
         '2': np.concatenate([np.full(1, 0.1), np.full(2, 1.0), np.full(1, 0.1)]),
+
     }
     V_imposto = {
         '1':  [[13, 0.0]]  ,
