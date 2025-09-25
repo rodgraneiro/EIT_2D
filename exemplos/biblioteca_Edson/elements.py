8df932be5a9d72430e3c3d8c274b589260453766
rodgraneiro <40045555+rodgraneiro@users.noreply.github.com>
2025-09-24 21:58:57 -0300

    colocando em elements.py a versao Edson KGeo triangle

--- exemplos/biblioteca_Edson/elements.py
+++ exemplos/biblioteca_Edson/elements.py
@@ -66,7 +66,46 @@ class LinearTriangle(MyElement):
 
     def __init__(self):
         super().__init__()
+    # Gera a matriz local dos elementos triangulares lineares com 3 nohs
+    # Equacionamento deduzido na tese do Fernando Moura, Apendice A.1.1
+    ###############################################################################
+    # Esta função calcula a matriz local de condutividade da malha de elementos
+    # finitos
+    #
+    #            sigma_i                                      
+    # Y_local = ------------- * [matriz 2d ***completar***]             
+    #             4*A_i                                             
+    ###############################################################################
+    # OBS: só parte geométrica (que nunca muda), sem multiplicar por sigma (que muda)
+    #def CalcKgeo(self, elem):
+        
+    def CalcKgeo(self):
+        noh1 = int(self.Topology[0])
+        noh2 = int(self.Topology[1])
+        noh3 = int(self.Topology[2])
+        print(noh1, noh2, noh3)
+        
+        x = [self.Coordinates[noh1][0], self.Coordinates[noh2][0], self.Coordinates[noh3][0]]
+        y = [self.Coordinates[noh1][1], self.Coordinates[noh2][1], self.Coordinates[noh3][1]]
 
+        triangulo = np.array([ [1, x[0],  y[0]], [1, x[1],  y[1]], [1, x[2],  y[2]]], dtype=np.float64)
+        area_triangulo = abs((np.linalg.det(triangulo))/2)
+    
+        C_11 =  ( y[1]-y[2])**2 + (x[2]-x[1])**2
+        C_12 =  ( y[1]-y[2])*(y[2]-y[0])+(x[2]-x[1])*(x[0]-x[2])          #A_21 = A_12
+        C_13 =  ( y[1]-y[2])*(y[0]-y[1])+(x[2]-x[1])*(x[1]-x[0])          #A_31 = A_13
+        C_22 =  (y[2]-y[0])**2 + (x[0]-x[2])**2                           #A_32 = A_23
+        C_23 =  (y[2]-y[0])*(y[1]- y[1])+(x[0]-x[2])*(x[1]-x[0])          # Na tese está: (Bt_m*Bt_o)+(Gm_m*Gm_o)+(Dt_m*Dt_o)
+        C_33 =  (y[0]- y[1])**2 + (x[1]-x[0])**2  
+
+        
+        self.KGeo = (self.Altura2D /(4.0*area_triangulo))*np.array([[C_11, C_12, C_13], 
+                                        [C_12, C_22, C_23],      
+                                        [C_13, C_23, C_33]       
+                                        ])
+
+        print('KGeo1', self.KGeo)
+    '''
     # Gera a matriz local dos elementos triangulares lineares com 3 nohs
     # Equacionamento deduzido na tese do Fernando Moura, Apendice A.1.1
     def CalcKgeo(self):
@@ -99,7 +138,7 @@ class LinearTriangle(MyElement):
                 value = (self.Altura2D / (4.0 * area)) * (coeficientes[0][i] * coeficientes[0][j] + coeficientes[1][i] * coeficientes[1][j])
                 self.KGeo[j][i] = value
                 self.KGeo[i][j] = value
-
+    '''
 # Equacionamento deduzido na tese do Fernando Moura, Apendice A.2.1
 class LinearLineHua(MyElement):
     
@@ -119,8 +158,8 @@ class LinearLineHua(MyElement):
             mtrz_lenth_a[i][1] =self.Coordinates[self.Topology[i]][1]
             node1 = self.Topology[i]
             #self.msh_physical_groups = self.__mshdata.cell_data_dict["gmsh:physical"][self.element_type]
-            print('self.msh_physical_groups ???', self.msh_physical_groups)
-            print('nodes', node1)
+            #print('self.msh_physical_groups ???', self.msh_physical_groups)
+            #print('nodes', node1)
             
         # comprimento 'a' sqtr( (x2-x1)^2 + (y2-y1)^2) )
 
@@ -131,4 +170,4 @@ class LinearLineHua(MyElement):
         
         # MATRIZ DE RIGIDEZ DO ELEMENTO Hua
         self.KGeo = ((self.Altura2D*lenth_a)/6)*mHua
-        print('KGeo', self.KGeo)
+        print('KGeo_Hua', self.KGeo)
