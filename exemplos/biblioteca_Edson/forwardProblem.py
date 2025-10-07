93243a201beea6efdeead5c4213e3416488b4dd3
rodgraneiro <40045555+rodgraneiro@users.noreply.github.com>
2025-10-06 21:55:10 -0300

    acrescentando funcoes na classe forwardProblem

--- exemplos/biblioteca_Edson/forwardProblem.py
+++ exemplos/biblioteca_Edson/forwardProblem.py
@@ -8,18 +8,20 @@ Created on Thu Oct  2 14:17:52 2025
 import numpy as np
 import mesh
 import elements
+import gmsh
+import sys
 
 
 class forward_problem: 
-    def __init__(self, mymesh: "mesh.MyMesh"):
+    def __init__(self, mymesh: "mesh.MyMesh", Pcorrente=None):
         if not hasattr(mymesh, "KGlobal"): # verifica se o objeto mymesh tem um atributo chamado KGlobal.
             raise TypeError("mymesh não tem atributo KGlobal (instancie MyMesh e calcule CalcKGlobal()).")
-        if not hasattr(mymesh, "corrente"): # verifica se o objeto mymesh tem um atributo chamado corrente.
-            raise TypeError("mymesh não tem atributo 'corrente' (defina um vetor de correntes).")
+        if Pcorrente is None:
+            raise TypeError("não tem atributo 'Pcorrente' (defina um vetor de correntes).")
 
         self.mymesh = mymesh
         self.KGlobal = np.asarray(mymesh.KGlobal, dtype=float)
-        self.corrente = np.asarray(mymesh.corrente, dtype=float)
+        self.corrente = Pcorrente
         self.Vmedido = None
 
 
@@ -73,4 +75,82 @@ class forward_problem:
                          vetor_correnteVM)
       return Vmedido                             # retorna valor observado/medido
 ###############################################################################
-  
+
+###############################################################################
+# Esta função cria arquivos .pos (Post-Processing) apara vizulização no Gmsh.
+#  
+###############################################################################
+    def criar_arquivo_pos_2D(self, matriz_coordenadas, matriz_topologia, 
+                          n_eletrodos, V_sol, nome_arquivo):
+        #for kk in range(n_eletrodos):
+        for kk in range(1):
+
+            
+            # 9) Gerar arquivo POS
+            
+            #nome_arquivo = 'frame_1_5_11.pos'
+            arquivo = open('D:/GIT_EIT_2D/EIT_2D/malhasPOS/'+ nome_arquivo + str(kk) + '.pos', 'w+')
+            arquivo.writelines(u'View "A list-based view" { \n')
+            
+            for i in range(0, len(matriz_topologia)):
+            
+                node_l = int(matriz_topologia[i][0]) # pegar dados do dataframe
+                node_m = int(matriz_topologia[i][1])
+                node_n = int(matriz_topologia[i][2])
+            
+                arquivo.writelines(u'ST(')
+              
+                arquivo.writelines([str(matriz_coordenadas[node_l][0]),',', 
+                                    str(matriz_coordenadas[node_l][1]),',0,'])
+                arquivo.writelines([str(matriz_coordenadas[node_m][0]),',', 
+                                    str(matriz_coordenadas[node_m][1]),',0,'])
+                arquivo.writelines([str(matriz_coordenadas[node_n][0]),',', 
+                                    str(matriz_coordenadas[node_n][1]),',0'])
+                
+                arquivo.writelines(u')')
+                arquivo.writelines(u'{')
+                arquivo.writelines([str(V_sol[node_l]),',', 
+                                    str(V_sol[node_m]),',', 
+                                    str(V_sol[node_n])])
+               
+            
+                arquivo.writelines(u'};')
+                arquivo.writelines(u'\n')
+            arquivo.writelines(u'};')
+            
+            arquivo.close()  
+###############################################################################    
+
+###############################################################################
+# Esta função abre arquivos .pos (Post-Processing), criados pela função
+# 'criar_arquivo_pos'  para vizulização no Gmsh.
+#  
+###############################################################################
+    def abrir_Gmsh_pos(self,nome_arquivo, n_eletrodos):
+        for pos in range(n_eletrodos):
+            # Inicialize o Gmsh
+            gmsh.initialize()
+            
+            # Carregue o arquivo .geo
+            gmsh.open('D:/GIT_EIT_2D/EIT_2D/malhasPOS/' + nome_arquivo + str(pos) + '.pos') #open('TesteBanana0.pos')
+            #gmsh.View[0].IntervalsType = 2;
+            #gmsh.option.setNumber("View["+str(pos)+"].IntervalsType", 1)
+            #gmsh.option.setNumber("View["+str(pos)+"].NbIso", 500)
+            # Obtenha as entidades do modelo
+            #entities = gmsh.model.getEntities()
+            #gmsh.model.geo.synchronize()
+            
+            #gmsh.model.mesh.generate(3)
+            #gmsh.option.setNumber("Mesh.MshFileVersion",2.2)   
+            #gmsh.write("poligono_2D_Rho_py.msh")
+            #gmsh.write("poligono_2D_Rho.geo_unrolled")
+            
+            
+            
+            #gmsh.model.mesh.generate(3)
+            # Feche o arquivo .geo
+            
+        if '-nopopup' not in sys.argv:
+            gmsh.fltk.run()
+        gmsh.finalize()
+###############################################################################      
