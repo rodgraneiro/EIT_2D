b505fe2509008a1763bbda3e2ee229a98a8dca24
rodgraneiro <40045555+rodgraneiro@users.noreply.github.com>
2026-02-09 18:10:14 -0300

    criando teste para elemento anisotropico

--- exemplos/biblioteca_Edson/teste_exemplos_biblioteca_Edson.py
+++ exemplos/biblioteca_Edson/teste_exemplos_biblioteca_Edson.py
@@ -42,7 +42,7 @@ fwd.abrir_Gmsh_pos(nome_arquivo, N_padraoCC)
 '''
 
 
-
+'''
 
 # TESTE COM HUA
 
@@ -101,5 +101,70 @@ N_padraoCC = 1
 fwd.criar_arquivo_pos_2D(N_padraoCC, fwd.Vmedido, nome_arquivo)
 
 fwd.abrir_Gmsh_pos(nome_arquivo, N_padraoCC)
+'''
+###############################################################################
+
 
 ###############################################################################
+######################## TESTE ANISOTROPIA ####################################
+###############################################################################
+# %%time
+import numpy as np
+import mesh
+import forwardProblem
+import inverseProblem
+import inverseProblem_2D
+import matplotlib.pyplot as plt
+
+# TESTE COM eletrodo pontual mesh 4 elementos
+
+###############################################################################
+#nome = '../../malhasMSH/quatro_triangulos_03nov2025.msh'
+nome = 'malhasMSH/dezesseis_triangulos_22jan25.msh'
+
+#MinhaMalhaPto2 = mesh.PointElectrodes2DMeshEdson(4, nome_msh=nome, altura2D = 1.00)
+MinhaMalhaPto2 = mesh.PointElectrodes2DMeshAnisotropic(4, nome_msh=nome, altura2D = 0.02)
+#MinhaMalhaPto2 = mesh.PointElectrodes2DMeshEdson(4, nome_msh=nome, altura2D = 0.02)
+#inearTriangleAnisotropic
+MinhaMalhaPto2.ReadMesh() 
+
+#print(MinhaMalhaPto2.Elements[2])
+#print(f"Centroid: {MinhaMalhaPto2.Elements[2].Centroid}")
+#print(f"KGeo: \n{MinhaMalhaPto2.Elements[2].KGeo}")
+#sigma_inicial = np.full(MinhaMalhaPto2.NumberOfElements, 1.0)          # Monta vetor sigma inicial
+
+
+
+meus_sigmas = {
+1000 : 1.0,
+1001 : 1.00}#,
+#1002 : 2.00,    
+#1003 : 2.00}
+
+MinhaMalhaPto2.SetSigmaPhysicaEntity(meus_sigmas)
+
+
+MinhaMalhaPto2.CalcKGlobal() # calculando KGlobal usando Sigmas
+
+
+#print(f'MinhaMalhaPto2.KGlobal =  {MinhaMalhaPto2.KGlobal}')
+
+fwd = forwardProblem.forward_problem(MinhaMalhaPto2, Pcorrente=None, SkipPattern=1, I =1.0e-3)   # __init__ roda aqui
+
+#print(f'Pcorrente \n {fwd.corrente[MinhaMalhaPto.NumberOfNodes-MinhaMalhaPto.NumberOfElectrodes: MinhaMalhaPto.NumberOfNodes]}')
+#print(f'Pcorrente \n {fwd.corrente[:16]}')
+
+#print(f'Pcorrente \n {fwd.corrente.shape}')
+
+mtz_Vmedido = fwd.Solve()
+#print(f'Vmedido \n {fwd.Vmedido[:,0]}')
+
+nome_arquivo = 'ParaVernoGmshPto'
+#fwd.criar_arquivo_pos_2D( fwd.Vmedido, nome_arquivo)
+
+#fwd.abrir_Gmsh_pos(nome_arquivo, runGmsh=True)
+
+#print(f'self.Yinversa \n {fwd.Yinversa}')
+
+V_measured = fwd.Vmedido_eletrodos
+print('V_measured ',V_measured)
