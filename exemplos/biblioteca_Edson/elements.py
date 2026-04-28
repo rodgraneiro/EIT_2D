05c49be8e1f13babab2690cd94c629e28cad81e2
rodgraneiro <40045555+rodgraneiro@users.noreply.github.com>
2026-04-28 01:41:43 -0300

    debug e teste do arquivos inverseProblem_2D_Anisotropic_Hua.py 27abr26

--- exemplos/biblioteca_Edson/elements.py
+++ exemplos/biblioteca_Edson/elements.py
@@ -200,7 +200,7 @@ class LinearLineHua(MyElement):
 
     def CalcKgeo(self):
 
-        zc = 0.001
+        zc = 1000.0
         #zc = self.mymesh.z_contact_eletrode 
         #print('zc =', zc)
         mtrz_lenth_a = np.zeros((2, 2), dtype=float)
