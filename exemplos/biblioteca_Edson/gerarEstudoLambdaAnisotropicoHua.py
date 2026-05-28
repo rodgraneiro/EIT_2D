acd4696fb4bd4f2ece3db002c21dfbd252ee7966
rodgraneiro <rodgraneiro@gmail.com>
2026-05-28 15:31:53 -0300

    rodando para testar noa pagina do site 26mai26_b

--- exemplos/biblioteca_Edson/gerarEstudoLambdaAnisotropicoHua.py
+++ exemplos/biblioteca_Edson/gerarEstudoLambdaAnisotropicoHua.py
@@ -171,7 +171,7 @@ def rodar_simulacao(lambda_val, html_name="resultado"):
     #htmlName = 'XXXrectangularHomogeneousAnisotropy30Neg'
     htmlName = nome_html
     invProblem_2D = inverseProblem_2D_Anisotropic_Hua.inverse_problem(MinhaMalha_base, Pcorrente=fwd.corrente)
-    invProblem_2D.solve(V_measured_phaton, initialEstimate=start,alpha =0.1,  Lambda = lambda_val, max_iter=1,Tol=1.0e-9, html_name = htmlName)
+    invProblem_2D.solve(V_measured_phaton, initialEstimate=start,alpha =0.1,  Lambda = lambda_val, max_iter=1,Tol=25.0e-9, html_name = htmlName)
     #print('Y_jacobian',invProblem.Y_jacobian)
 
 #sigma_inicial_cont = np.loadtxt("sigma_inicial_cont.txt")
